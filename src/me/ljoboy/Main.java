5037679ff282c251e81d869d90d1bd524231597c
Dark Angel <jonathanyombo@gmail.com>
2021-05-15 23:40:16 +0200

    debugging

--- src/me/ljoboy/Main.java
+++ src/me/ljoboy/Main.java
@@ -1,10 +1,38 @@
 package me.ljoboy;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        Product p1 = new Product();
-        Product p2 = new Product();
+        Scanner in = new Scanner(System.in);
+        int tempQty, tempNumber;
+        String tempName;
+        float tempPrice;
+
+        System.out.println("Nom ==> ");
+        tempName = in.nextLine();
+        System.out.println("Numero ==> ");
+        tempNumber = in.nextInt();
+        System.out.println("Prix ==> ");
+        tempPrice = in.nextFloat();
+        System.out.println("Quantité en stock ==> ");
+        tempQty = in.nextInt();
+
+        Product p1 = new Product(tempNumber, tempName, tempQty, tempPrice);
+
+        in.nextLine();
+
+        System.out.println("Nom ==> ");
+        tempName = in.nextLine();
+        System.out.println("Numero ==> ");
+        tempNumber = in.nextInt();
+        System.out.println("Prix ==> ");
+        tempPrice = in.nextFloat();
+        System.out.println("Quantité en stock ==> ");
+        tempQty = in.nextInt();
+
+        Product p2 = new Product(tempNumber, tempName, tempQty, tempPrice);
 
         Product p3 = new Product(3, "Disque dur", 500, 80f);
         Product p4 = new Product(4, "Stylo", 270, 1f);
