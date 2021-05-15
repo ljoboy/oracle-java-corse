4aaffa07705cdd8d50ab3fe275ef98bf28dd1d59
Dark Angel <jonathanyombo@gmail.com>
2021-05-16 01:08:13 +0200

    6_4

--- src/me/ljoboy/Main.java
+++ src/me/ljoboy/Main.java
@@ -1,11 +1,32 @@
 package me.ljoboy;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        int maxSize = -1;
         Scanner in = new Scanner(System.in);
+
+        do {
+            try {
+                System.out.println("Saisissez le nombre de produits à ajouter");
+                System.out.println("Saisissez 0 (zéro) si vous ne souhaiter pas ajouter de produits : ");
+                maxSize = in.nextInt();
+
+                if (maxSize < 0) {
+                    System.out.println("Valeur incorrecte saisie.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Type de données incorrect saisi");
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            in.nextLine();
+        } while (maxSize < 0);
+
         int tempQty, tempNumber;
         String tempName;
         float tempPrice;
