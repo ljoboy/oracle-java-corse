4278d74ebacdd5e230822ec307f65625ffb2cde8
Dark Angel <jonathanyombo@gmail.com>
2021-05-16 01:59:11 +0200

    6 Okay...

--- src/me/ljoboy/Main.java
+++ src/me/ljoboy/Main.java
@@ -21,50 +21,67 @@ public class Main {
 
             } catch (InputMismatchException e) {
                 System.out.println("Type de données incorrect saisi");
+                in.nextLine();
             } catch (Exception e) {
                 System.out.println(e);
+                in.nextLine();
             }
-            in.nextLine();
         } while (maxSize < 0);
 
-        int tempQty, tempNumber;
-        String tempName;
-        float tempPrice;
+        if (maxSize == 0) {
+            System.out.println("Aucun produit n'est requis");
+        } else {
+            Product[] products = new Product[maxSize];
+            
+            int tempQty, tempNumber;
+            String tempName;
+            float tempPrice;
+            
+            for (int i = 0; i < maxSize; i++) {
+                in.nextLine();
 
-        System.out.println("Nom ==> ");
-        tempName = in.nextLine();
-        System.out.println("Numero ==> ");
-        tempNumber = in.nextInt();
-        System.out.println("Prix ==> ");
-        tempPrice = in.nextFloat();
-        System.out.println("Quantité en stock ==> ");
-        tempQty = in.nextInt();
+                System.out.println("Nom ==> ");
+                tempName = in.nextLine();
+                System.out.println("Numero ==> ");
+                tempNumber = in.nextInt();
+                System.out.println("Prix ==> ");
+                tempPrice = in.nextFloat();
+                System.out.println("Quantité en stock ==> ");
+                tempQty = in.nextInt();
 
-        Product p1 = new Product(tempNumber, tempName, tempQty, tempPrice);
+                products[i] = new Product(tempNumber, tempName, tempQty, tempPrice);
+            }
 
-        in.nextLine();
+            in.close();
 
-        System.out.println("Nom ==> ");
-        tempName = in.nextLine();
-        System.out.println("Numero ==> ");
-        tempNumber = in.nextInt();
-        System.out.println("Prix ==> ");
-        tempPrice = in.nextFloat();
-        System.out.println("Quantité en stock ==> ");
-        tempQty = in.nextInt();
+            for (Product p : products) {
+                System.out.println(p);
+            }
+        }
 
-        Product p2 = new Product(tempNumber, tempName, tempQty, tempPrice);
+        
 
-        Product p3 = new Product(3, "Disque dur", 500, 80f);
-        Product p4 = new Product(4, "Stylo", 270, 1f);
-        Product p5 = new Product(5, "Bracelet", 200, 2.5f);
-        Product p6 = new Product(6, "Bague", 250, 1.5f);
-
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p3);
-        System.out.println(p4);
-        System.out.println(p5);
-        System.out.println(p6);
+//        System.out.println("Nom ==> ");
+//        tempName = in.nextLine();
+//        System.out.println("Numero ==> ");
+//        tempNumber = in.nextInt();
+//        System.out.println("Prix ==> ");
+//        tempPrice = in.nextFloat();
+//        System.out.println("Quantité en stock ==> ");
+//        tempQty = in.nextInt();
+//
+//        Product p2 = new Product(tempNumber, tempName, tempQty, tempPrice);
+//
+//        Product p3 = new Product(3, "Disque dur", 500, 80f);
+//        Product p4 = new Product(4, "Stylo", 270, 1f);
+//        Product p5 = new Product(5, "Bracelet", 200, 2.5f);
+//        Product p6 = new Product(6, "Bague", 250, 1.5f);
+//
+//        System.out.println(p1);
+//        System.out.println(p2);
+//        System.out.println(p3);
+//        System.out.println(p4);
+//        System.out.println(p5);
+//        System.out.println(p6);
     }
 }
