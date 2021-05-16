d3668782592612845f81ad3dbeb5ee3c7a25dc9e
Dark Angel <jonathanyombo@gmail.com>
2021-05-17 00:33:48 +0200

    getter and setter

--- src/me/ljoboy/DVD.java
+++ src/me/ljoboy/DVD.java
@@ -4,4 +4,28 @@ public class DVD extends Product {
     private int duree;
     private int age;
     private String studio;
+
+    public int getDuree() {
+        return duree;
+    }
+
+    public void setDuree(int duree) {
+        this.duree = duree;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getStudio() {
+        return studio;
+    }
+
+    public void setStudio(String studio) {
+        this.studio = studio;
+    }
 }
