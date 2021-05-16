f49b7f54272bba3a05fb7adac842bbe099b42041
Dark Angel <jonathanyombo@gmail.com>
2021-05-17 00:39:46 +0200

    constructor

--- src/me/ljoboy/CD.java
+++ src/me/ljoboy/CD.java
@@ -5,6 +5,13 @@ public class CD extends Product {
     private int nb_titres;
     private String mson_Disque;
 
+    public CD(int numero, String nom, int quantite, float prix, String artiste, int nb_titres, String mson_Disque) {
+        super(numero, nom, quantite, prix);
+        Artiste = artiste;
+        this.nb_titres = nb_titres;
+        this.mson_Disque = mson_Disque;
+    }
+
     public String getArtiste() {
         return Artiste;
     }
