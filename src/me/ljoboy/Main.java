d4c1ebaa3feba499475f428d9eebf79109324d27
Dark Angel <jonathanyombo@gmail.com>
2021-05-16 23:26:00 +0200

    next step 4.b

--- src/me/ljoboy/Main.java
+++ src/me/ljoboy/Main.java
@@ -51,6 +51,35 @@ public class Main {
 //        System.out.println(p6);
     }
 
+    public static int getMenuOption(Scanner in) {
+        int choix = -1;
+        do {
+            try {
+                System.out.println("1. Afficher l'inventaire");
+                System.out.println("2. Ajouter une quantité de stock");
+                System.out.println("3. Déduire une quantité de stock");
+                System.out.println("4. Ne pas réapprovisionner le produit");
+                System.out.println("0. Quitter");
+                System.out.println("Veuillez saisir une option de menu : ");
+
+                choix = in.nextInt();
+                if (choix < 0 || choix > 4) {
+                    System.out.println("Valeur incorrecte saisie.");
+                }
+
+            } catch (InputMismatchException e) {
+                System.out.println("Type de données incorrect saisi");
+                in.nextLine();
+            } catch (Exception e) {
+                System.out.println(e);
+                in.nextLine();
+            }
+
+        } while (choix < 0 || choix > 4);
+
+        return choix;
+    }
+
     public static void displayInventory(Product[] products) {
         for (Product p : products) {
             System.out.println(p);
